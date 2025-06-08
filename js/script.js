bf760094e01b93d4c94a48ef4db25155fa568126
Egor <pattrixd@yandex.by>
2025-06-09 00:08:33 +0300

    Carousel indicators

--- js/script.js
+++ js/script.js
@@ -184,27 +184,11 @@ document.addEventListener("DOMContentLoaded", () => {
     return await res.json();
   };
 
-  const getResorse = async (URL) => {
-    const res = await fetch(URL);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch${url},status ${res.status}`);
-    }
-
-    return await res.json();
-  };
-  getResorse("http://localhost:3000/menu").then((data) => {
-    data.forEach(({ img, altimg, title, descr, price }) => {
-      new Card(
-        img,
-        altimg,
-        title,
-        descr,
-        price,
-        ".menu .container"
-      ).render();
-    });
-  });
+  axios.get("http://localhost:3000/menu").then((data) =>
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
+      new Card(img, altimg, title, descr, price, ".menu .container").render();
+    })
+  );
 
   function bindPostData(form) {
     form.addEventListener("submit", (e) => {
@@ -255,7 +239,101 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 2000);
   }
 
-  fetch("http://localhost:3000/menu")
-    .then((data) => data.json())
-    .then((res) => console.log(res));
+  //Slider
+
+  const sliders = document.querySelectorAll(".offer__slide"),
+    current = document.querySelector("#current"),
+    nextButton = document.querySelector(".offer__slider-next"),
+    sliderWrapper = document.querySelector(".offer__slider-wrapper"),
+    sliderField = document.querySelector(".offer__slider-inner"),
+    width = window.getComputedStyle(sliderWrapper).width,
+    prevButton = document.querySelector(".offer__slider-prev");
+
+  let currentValue = 1,
+    offset = 0;
+
+  sliderField.style.width = sliders.length * 100 + "%";
+  sliderField.style.display = "flex";
+  sliderField.style.transition = "0.5s all";
+
+  sliderWrapper.style.overflow = "hidden";
+
+  showCurrentSlider();
+  document.querySelector("#total").textContent =
+    sliders.length < 10 ? `0${sliders.length}` : sliders.length;
+
+  sliders.forEach((item) => {
+    item.style.width = width;
+  });
+
+  nextButton.addEventListener("click", () => {
+    showCurrentSlider("+");
+    if (offset === parseInt(width) * (sliders.length - 1)) {
+      offset = 0;
+    } else {
+      offset = parseInt(width) * (currentValue - 1);
+    }
+    sliderField.style.transform = `translateX(-${offset}px)`;
+  });
+  prevButton.addEventListener("click", () => {
+    showCurrentSlider("-");
+    if (offset === 0) {
+      offset = parseInt(width) * (sliders.length - 1);
+    } else {
+      offset = parseInt(width) * (currentValue - 1);
+    }
+
+    sliderField.style.transform = `translateX(-${offset}px)`;
+  });
+
+  function showCurrentSlider(str = "") {
+    if (str == "+") {
+      currentValue = currentValue === sliders.length ? 1 : currentValue + 1;
+    } else if (str == "-") {
+      currentValue = currentValue === 1 ? 4 : currentValue - 1;
+    }
+    showCurrentDot();
+    current.textContent = currentValue < 10 ? `0${currentValue}` : currentValue;
+  }
+
+  //Navigation Slider
+
+  const dotsWrapper = document.createElement("div"),
+    slide = document.querySelector(".offer__slider");
+
+  dotsWrapper.classList.add("carousel-indicators");
+  slide.append(dotsWrapper);
+  slide.style.position = "relative";
+
+  for (let i = 0; i < sliders.length; i++) {
+    let dot = document.createElement("div");
+
+    dot.classList.add("dot");
+    dotsWrapper.append(dot);
+  }
+
+  showCurrentDot();
+
+  function showCurrentDot() {
+    let dots = document.querySelectorAll(".dot");
+    dots.forEach((item, index) => {
+      if (index === currentValue - 1) {
+        item.classList.add("dot-active");
+      } else {
+        item.classList.remove("dot-active");
+      }
+    });
+  }
+  dotsWrapper.addEventListener("click", (e) => {
+    if (e.target && e.target.classList.contains("dot")) {
+      document.querySelectorAll(".dot").forEach((item, index) => {
+        if (e.target == item) {
+          currentValue = index + 1;
+          showCurrentSlider();
+          offset = parseInt(width) * index;
+          sliderField.style.transform = `translateX(-${offset}px)`;
+        }
+      });
+    }
+  });
 });
